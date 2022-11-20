82f57282c58f4fd5b5dd9976d4bdc92cac1240ff
TempestZYTux <2797643408@qq.com>
2022-11-20 23:53:21 +0800

    add hit render +1

--- src/main/java/tempestissimo/club/arcaea/utils/HitCalculator.java
+++ src/main/java/tempestissimo/club/arcaea/utils/HitCalculator.java
@@ -1,4 +1,27 @@
 package tempestissimo.club.arcaea.utils;
 
-public class HitCaculator {
+import org.bukkit.Note;
+import org.bukkit.configuration.file.FileConfiguration;
+import tempestissimo.club.arcaea.Arcaea4pigot;
+import tempestissimo.club.arcaea.utils.entities.Song;
+import tempestissimo.club.arcaea.utils.entities.hit_related.HitJob;
+
+import java.util.ArrayList;
+
+public class HitCalculator {
+    public Arcaea4pigot plugin;
+    public FileConfiguration config;
+
+    public ArrayList<HitJob> compileHit(Integer songIndex,Integer ratingClass){
+        ArrayList<HitJob> results = new ArrayList<>();
+        Song song = plugin.songScanner.song_list.get(songIndex);
+        return results;
+    }
+
+    public ArrayList<HitJob> compileNote(ArrayList<Note> notes, String timingGroupPrefix){
+        ArrayList<HitJob> results = new ArrayList<>();
+
+        return results;
+    }
+
 }
--- src/main/java/tempestissimo/club/arcaea/utils/entities/hit_related/HitJob.java
+++ src/main/java/tempestissimo/club/arcaea/utils/entities/hit_related/HitJob.java
@@ -1,2 +1,30 @@
-package tempestissimo.club.arcaea.utils.entities.hit_related;public class HitJob {
+package tempestissimo.club.arcaea.utils.entities.hit_related;
+
+/**
+ * 判定时间
+ */
+public class HitJob {
+    public Long frame;
+    public Double x;
+    public Double y;
+    public Double z;
+
+    public HitJob(Long frame, Double x, Double y, Double z) {
+        this.frame = frame;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public HitJob() {}
+
+    @Override
+    public String toString() {
+        return "HitJob{" +
+                "frame=" + frame +
+                ", x=" + x +
+                ", y=" + y +
+                ", z=" + z +
+                '}';
+    }
 }
